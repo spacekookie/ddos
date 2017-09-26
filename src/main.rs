b812fb3be82f7493bcf91dfa323805cd663c4690
Katharina Fey <kookie@spacekookie.de>
2017-09-26 14:56:42 +0200

    Changing CLI. Now using a toml config

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,11 @@ struct DDOS {
     api_port: u32,
 }
 
+static DEF_PORT: &'static str = "8001";
+static DEF_CONFIG: &'static str = "ddos.lua";
+static DEF_HOSTS: &'static str = "hosts.json";
+static DEF_KEYS: &'static str = "keys/";
+
 /// Main application entry point
 fn main() {
     let name = env::args().next()
@@ -37,10 +42,10 @@ fn main() {
     let matches = app.get_matches();
 
     /* Get argument values or set some sane defaults */
-    let port = matches.value_of("port").unwrap_or("8001");
-    let config: String = matches.value_of("config").unwrap_or("ddos.lua").to_string();
-    let hoststore: String = matches.value_of("hoststore").unwrap_or("hosts.json").to_string();
-    let keystore: String = matches.value_of("keystore").unwrap_or("keys/").to_string();
+    let port = matches.value_of("port").unwrap_or(DEF_PORT);
+    let config: String = matches.value_of("config").unwrap_or(DEF_CONFIG).to_string();
+    let hoststore: String = matches.value_of("hoststore").unwrap_or(DEF_HOSTS).to_string();
+    let keystore: String = matches.value_of("keystore").unwrap_or(DEF_KEYS).to_string();
 
     // println!("Port: {}", port);
     // println!("Config: {}", config);
