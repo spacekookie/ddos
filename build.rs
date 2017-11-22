5f0364550ec4cb29a21cabb375817d161f3e6113
Katharina Fey <kookie@spacekookie.de>
2017-11-22 12:44:04 +0100

    Fixing the cmake build?!

--- build.rs
+++ build.rs
@@ -1,15 +1,11 @@
 extern crate cmake;
-// extern crate cc;
 
 fn main() {
     let dst = cmake::build("libluadns");
+
     println!("cargo:rustc-link-search=native={}", dst.display());
     println!("cargo:rustc-link-lib=static=luadns");
-    
-    // cc::Build::new()
-        // .warnings(false)
-        // .file("libluadns/luadnsd.c")
-        // .include("/usr/include/luajit-2.0")
-        // .flag("-lluajit-5.1")
-        // .compile("luadns.so");
+
+    println!("cargo:rustc-link-search=native=/usr/lib");
+    println!("cargo:rustc-link-lib=luajit-5.1");
 }
